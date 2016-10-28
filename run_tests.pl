8c3c13e362576a0bde45b0e67323233797219407
mirror <mirror12k@gmail.com>
2016-10-28 01:40:03 -0400

    - reimplemented tester file for Snow::Lua using new Sugar::Test::Barrage

--- run_tests.pl
+++ run_tests.pl
@@ -4,35 +4,15 @@ use warnings;
 
 use feature 'say';
 
+use Sugar::Test::Barrage;
+
 
 my $opts = join ' ', @ARGV;
 
-foreach my $file (<lua_test_files/*>) {
-	say "test file $file";
-	my @lua_lines = `lua $file`;
-	my @snow_lines = `perl $opts test.pl $file`;
-
-	my $error = 0;
-	if (@snow_lines != @lua_lines) {
-		say "\tincorrect number of lines $#lua_lines vs $#snow_lines";
-		$error = 1;
-	}
-	foreach (0 .. $#lua_lines) {
-		unless (defined $snow_lines[$_] and $lua_lines[$_] eq $snow_lines[$_]) {
-			say "\tinconsistent lines [$_]";
-			print "\t\t$lua_lines[$_]" if defined $lua_lines[$_];
-			print "\t\t$snow_lines[$_]" if defined $snow_lines[$_];
-
-			$error = 1;
-		}
-	}
-
-	if ($error) {
-		say "test $file failed";
-	} else {
-		say "test $file successful";
-	}
-}
-
+Sugar::Test::Barrage->new(
+	test_files_dir => 'lua_test_files',
+	control_processor => "lua \$testfile",
+	test_processor => "perl $opts test.pl \$testfile",
+)->run;
 
 

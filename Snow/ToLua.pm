c31dad87b9ec15487ab91ccf0b6799253f3bdb10
mirror <mirror12k@gmail.com>
2016-11-05 13:07:44 -0400

    - implemented a single function for compilation and a module for command line compilation

---
+++ Snow/ToLua.pm
@@ -0,0 +1,30 @@
+#!/usr/bin/env perl
+package Snow::ToLua;
+use strict;
+use warnings;
+
+use feature 'say';
+
+use Snow::SyntaxTranslator;
+use Snow::Lua::SyntaxStringer;
+
+
+
+sub compile_snow_to_lua {
+	my ($filepath) = @_;
+
+	my $parser = Snow::SyntaxTranslator->new(filepath => $filepath);
+	$parser->parse;
+	my $stringer = Snow::Lua::SyntaxStringer->new;
+	say $stringer->to_string($parser->{syntax_tree});
+}
+
+
+
+sub main {
+	compile_snow_to_lua $_ foreach @_;
+}
+
+
+
+caller or main(@ARGV);
